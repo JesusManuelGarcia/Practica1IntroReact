d3f3fb0944a03367b7b815e80a9dc7b4c1a23ff3
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-04 19:26:04 -0600

    declaracion de variable al clickear

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -118,3 +118,23 @@ class Square extends React.Component {
     );
   }
 }
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}

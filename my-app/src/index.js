add7917db5a84194f509fe698f7fb67f96c12cca
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-04 19:14:11 -0600

    modificacion de index agregando react components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -64,3 +64,19 @@ class Square extends React.Component {
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(<Game />);
   
+  class ShoppingList extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+          <h1>Lista de compras para {this.props.name}</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      );
+    }
+  }
+  
+  // Uso de ejemplo: <ShoppingList name="Mark" />

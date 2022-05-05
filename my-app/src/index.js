6cc3ce7e46022a868dfa92611513cbd64b035d56
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-04 19:53:14 -0600

    implementacion de viaje en el tiempo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -570,3 +570,13 @@ render() {
   );
 }
 <li key={user.id}>{user.name}: {user.taskCount} tasks left</li>
+const moves = history.map((step, move) => {
+  const desc = move ?
+    'Go to move #' + move :
+    'Go to game start';
+  return (
+    <li key={move}>
+      <button onClick={() => this.jumpTo(move)}>{desc}</button>
+    </li>
+  );
+});

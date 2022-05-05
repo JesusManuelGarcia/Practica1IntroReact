4f46c118a1fdf0fdc500ad1139b48a66116459f9
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-04 19:24:38 -0600

    segundo square para cambios

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -101,3 +101,20 @@ class Square extends React.Component {
     );
   }
 }
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => console.log('click')}>
+        {this.props.value}
+      </button>
+    );
+  }
+}

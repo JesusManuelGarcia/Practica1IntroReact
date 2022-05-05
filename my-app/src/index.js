366cab307905b11ec01532fe9c8de64e18dbc7e2
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-04 19:42:07 -0600

    elevando el estado

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -389,3 +389,28 @@ class Board extends React.Component {
             },
             // ...
           ]
+          class Game extends React.Component {
+            constructor(props) {
+              super(props);
+              this.state = {
+                history: [{
+                  squares: Array(9).fill(null),
+                }],
+                xIsNext: true,
+              };
+            }
+          
+            render() {
+              return (
+                <div className="game">
+                  <div className="game-board">
+                    <Board />
+                  </div>
+                  <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                  </div>
+                </div>
+              );
+            }
+          }

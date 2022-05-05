83f565532fced952dd335bbab556f9bc45da4de3
JesúsManuelGarcia <jesus-manuel-gar2000@hotmail.com>
2022-05-04 19:22:23 -0600

    agregando square

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -91,3 +91,13 @@ class Board extends React.Component {
     return <Square value={i} />;
   }
 }
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
